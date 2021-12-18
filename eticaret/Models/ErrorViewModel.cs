7314d7f30e8d4d5614cd16c1ae1a197baf81a743
abraham8755 <ibrahim.sahin5@org.sakarya.edu.tr>
2021-12-18 16:58:04 +0300

    theme
    18.12.2021

---
+++ eticaret/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace proje0._3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
